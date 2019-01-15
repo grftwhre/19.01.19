9ccf9f533e4ff2e527d253d9d46e16e8e5213e8b
grftwhre <44138690+grftwhre@users.noreply.github.com>
2019-01-16 00:48:44 +0300

    Add files via upload

---
+++ w12.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+int N,s;
+int maximum;
+printf("enter N\n");
+scanf("%d",&N);
+int a[N];
+int b[N];
+    for (int l=1;l<=N;l++)
+        scanf("%d",&a[l]);
+    for (int m=1;m<=N;m++)
+      b[m]=0;
+    for (int j=1;j<=N;j++)
+    {
+        s=j+5;
+        for (s;s<=N;s++)
+            b[j]=a[j]+a[s];
+    }
+    maximum=b[1];
+    for (int d=1;d<=N;d++)
+    {
+        if (b[d]>maximum)
+            maximum=b[d];
+    }
+    printf("%d",maximum);
+}
